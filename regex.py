452326453ddb6990d81e1a6b5a478da72f8769cb
Mohit Garg <matpsycic@gmail.com>
2016-06-18 18:45:07 -0400

    Added the substitution code in regex file

--- regex.py
+++ regex.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
+import re
 
+def main():
+    fh = open('raven.txt')
+    for line in fh:
+        if re.search('(Len|Neverm)ore',line):
+            print(line)
+    for line in fh:
+        print(re.sub('(Len|Neverm)ore','####',line))
+
+
+
+if __name__=="__main__":main()
